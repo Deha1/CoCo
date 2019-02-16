15090ce58b6526264193c989caf872b58c9995ad
wsh <985194583@qq.com>
2019-02-16 11:07:27 +0800

    test

---
+++ coco-parent/coco-manager/coco-mapper/src/main/java/com/xzy/mapper/CoAddressMapper.java
@@ -0,0 +1,16 @@
+package com.xzy.mapper;
+
+import com.xzy.pojo.CoAddress;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 武思浩
+ * @since 2019-02-16
+ */
+public interface CoAddressMapper extends BaseMapper<CoAddress> {
+
+}
